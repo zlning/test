62ff5b68821fa676d39f6c74f9f66c40a6d7a844
zlning <1730386018@qq.com>
2014-10-13 10:37:53 +0800

    head.s

--- head.s
+++ head.s
@@ -1,5 +1,5 @@
 fjkdjsflksdjlkfjdslkfjdlskjfldsjfldsjfldskjflkdsjflksjflksdjflkdskjflkdsjflkdjsflkjdslkfjldsjfldsj;fjdsflkjdslfjdslkfjlkdjfldkjflkdsjfl
-# Please enter the commit message for your changes. Lines starting
+#kdjsalkdjlka Please enter the commit message for your changes. Lines starting
 # with '#' will be ignored, and an empty message aborts the commit.
 # On branch experimental
 # Changes to be committed:
