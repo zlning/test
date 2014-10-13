cb72a95a8ef56b8e407819faf7d51e7442908dc6
郑力宁 <1730386018@qq.com>
2014-10-13 20:27:37 +0800

    Update head.s

--- head.s
+++ head.s
@@ -1,14 +1,1 @@
-fjkdklfksdfklsdjfkldidfdsfsdklfkjcnvohesflksdjlkfjdslkfjdlskjfldsjfldsjfldskjflkdsjflksjflksdjflkdskjflkdsjflkdjsflkjdslkfjldsjfldsj;fjdsflkjdslfjdslkfjlkdjfldkjflkdsjfl
-<<<<<<< HEAD
-#kdjsalds jfkljdskfljs dfkjdslkjflksfjlkjkdjlka Please enter the commit message for your changes. Lines starting
-=======
-ajdlksajdlkPlease enter the commit message for your changes. Lines starting
->>>>>>> thi
-# with '#' will be ignored, and an empty message aborts the commit.
-# On branch experimental
-# Changes to be committed:
-#   (use "git reset HEAD <file>..." to unstage)
-#
-#	modified:   head.s
-#	new file:   setup.s
-#wtcfs
+fuck znn
